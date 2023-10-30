bca8e92d33055dec6f9be7d6420f4dcf396fe387
UTKARSH SINGH <usjadon19@gmail.com>
2023-10-31 01:01:41 +0530

    v1.0.0

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:get/get.dart';
 import 'package:sketch_ar_overlay_app/controllers/app_controller.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({super.key});
@@ -26,6 +27,14 @@ class _CameraScreenState extends State<CameraScreen> {
             ArCoreView(
               onArCoreViewCreated: (ArCoreController controller) {
                 String imageUrl = appController.getImageUrl();
+                controller.addArCoreNode(
+                  ArCoreReferenceNode(
+                    name: 'Sketchfab',
+                    objectUrl: imageUrl,
+                    scale: Vector3(0.2, 0.2, 0.2),
+                    position: Vector3.zero(),
+                  ),
+                );
               },
             ),
           ],
